5c31339ea53116a964ad99723043f141e3d50b79
Brent Whitehead <brent.whitehead@skywest.com>
2019-07-02 16:06:17 -0600

    readded build for dev

--- antMain.bat
+++ antMain.bat
@@ -4,26 +4,27 @@ PUSHD %~dp0
 :begin
 
 CLS
-ECHO a. AntRunner/xPath     b. Restart tomcat
+ECHO a. AntRunner/xPath     b. Build for Dev
 ECHO s. Copy script         w. Watch css
-ECHO d. Timer
+ECHO d. Timer               t. Restart tomcat
 ECHO f. Open ContentURL
 
-CHOICE /C asdfbw /CS /N /M "Pick a task: "
+CHOICE /C asdfbwt /CS /N /M "Pick a task: "
 
 
 IF "%ERRORLEVEL%"== "1" START antRunner.bat
 IF "%ERRORLEVEL%"== "2" START Copy.bat
 IF "%ERRORLEVEL%"== "3" START Timer.bat
 IF "%ERRORLEVEL%"== "4" CALL ContentUrl.bat
-IF "%ERRORLEVEL%"== "5" GOTO tomcat
+IF "%ERRORLEVEL%"== "5" START runForDev.bat
 IF "%ERRORLEVEL%"== "6" START csswatch.bat
+IF "%ERRORLEVEL%"== "7" GOTO tomcat
 GOTO begin
 
 :tomcat 
-CALL net stop tomcat8
+net stop tomcat8
 IF ERRORLEVEL 1 ECHO this is stopped && PAUSE
-CALL net start tomcat8
+net start tomcat8
 PAUSE
 GOTO begin
 
