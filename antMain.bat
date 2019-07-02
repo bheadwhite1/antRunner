02837cac0ea9a94e9ba8f89d0629c0aa1d037aac
Brent Whitehead <brent.whitehead@skywest.com>
2019-07-02 13:54:23 -0600

    removed run for dev and replaced with restart tomcat

--- antMain.bat
+++ antMain.bat
@@ -4,7 +4,7 @@ PUSHD %~dp0
 :begin
 
 CLS
-ECHO a. AntRunner/xPath     b. Build for Dev
+ECHO a. AntRunner/xPath     b. Restart tomcat
 ECHO s. Copy script         w. Watch css
 ECHO d. Timer
 ECHO f. Open ContentURL
@@ -16,6 +16,14 @@ IF "%ERRORLEVEL%"== "1" START antRunner.bat
 IF "%ERRORLEVEL%"== "2" START Copy.bat
 IF "%ERRORLEVEL%"== "3" START Timer.bat
 IF "%ERRORLEVEL%"== "4" CALL ContentUrl.bat
-IF "%ERRORLEVEL%"== "5" START runForDev.bat
+IF "%ERRORLEVEL%"== "5" GOTO tomcat
 IF "%ERRORLEVEL%"== "6" START csswatch.bat
 GOTO begin
+
+:tomcat 
+CALL net stop tomcat8
+IF ERRORLEVEL 1 ECHO this is stopped && PAUSE
+CALL net start tomcat8
+PAUSE
+GOTO begin
+
