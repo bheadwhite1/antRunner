613fb846f983e6905249211850127274fbbf9ec0
bheadwhite <brent.whitehead@skywest.com>
2019-08-19 14:28:56 -0600

    updated to exit

--- shortInspec.bat
+++ shortInspec.bat
@@ -6,3 +6,4 @@ SET "techuserDir=C:\git\skywest-techuser"
 CALL ant "-DinputManual=%~1" "-Dpname=chapters" "-Dchapters=%~2" "-Diname=inspections" "-Dinspections=%~5" -f !techuserDir!\doctypes\\%3\transform\apache.ant %~4
 START /b "" CSCRIPT alert.vbs "%~1" runner is complete" "%~1"
 PAUSE
+EXIT
