8609afed6919de70fbc8f0e490c29bab4c3cccd9
bheadwhite <brent.whitehead@skywest.com>
2019-08-23 15:26:48 -0600

    clean antMain

--- antMain.bat
+++ antMain.bat
@@ -4,19 +4,17 @@ PUSHD %~dp0
 :begin
 
 CLS
-ECHO a. AntRunner/xPath     
+ECHO a. AntRunner/xPath    t. Toggle Tomcat 
 ECHO s. Watch css
 ECHO d. Timer
 ECHO f. Open ContentURL
 
-CHOICE /C asdfbwt /CS /N /M "Pick a task: "
+CHOICE /C asdft /CS /N /M "Pick a task: "
 IF [%ERRORLEVEL%] == [1] START antRunner.bat
-IF [%ERRORLEVEL%] == [2] CALL copyLocal.bat
+IF [%ERRORLEVEL%] == [2] START csswatch.bat
 IF [%ERRORLEVEL%] == [3] START Timer.bat
 IF [%ERRORLEVEL%] == [4] CALL ContentUrl.bat
-IF [%ERRORLEVEL%] == [5] START runForDev.bat
-IF [%ERRORLEVEL%] == [6] START csswatch.bat
-IF [%ERRORLEVEL%] == [7] GOTO tomcat
+IF [%ERRORLEVEL%] == [5] GOTO tomcat
 GOTO begin
 
 :tomcat
