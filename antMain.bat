43d68d38fe0ad665ca8db728165192219114c997
bheadwhite <brent.whitehead@skywest.com>
2019-08-23 14:47:02 -0600

    updated main

--- antMain.bat
+++ antMain.bat
@@ -10,15 +10,13 @@ ECHO d. Timer               t. Toggle tomcat
 ECHO f. Open ContentURL
 
 CHOICE /C asdfbwt /CS /N /M "Pick a task: "
-
-
-IF "%ERRORLEVEL%"== "1" START antRunner.bat
-IF "%ERRORLEVEL%"== "2" START copyLocal.bat
-IF "%ERRORLEVEL%"== "3" START Timer.bat
-IF "%ERRORLEVEL%"== "4" CALL ContentUrl.bat
-IF "%ERRORLEVEL%"== "5" START runForDev.bat
-IF "%ERRORLEVEL%"== "6" START csswatch.bat
-IF "%ERRORLEVEL%"== "7" GOTO tomcat
+IF [%ERRORLEVEL%] == [1] START antRunner.bat
+IF [%ERRORLEVEL%] == [2] CALL copyLocal.bat
+IF [%ERRORLEVEL%] == [3] START Timer.bat
+IF [%ERRORLEVEL%] == [4] CALL ContentUrl.bat
+IF [%ERRORLEVEL%] == [5] START runForDev.bat
+IF [%ERRORLEVEL%] == [6] START csswatch.bat
+IF [%ERRORLEVEL%] == [7] GOTO tomcat
 GOTO begin
 
 :tomcat
